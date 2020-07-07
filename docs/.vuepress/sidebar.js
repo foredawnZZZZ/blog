244784a19baabb8192d75d26cd341d7ff58c4dd7
foredawnZZZZ <18602151918@163.com>
2020-07-08 00:26:30 +0800

    07-08 add 项目与业务

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,5 +1,6 @@
 module.exports = {
   '/Command/': require('../Command/sidebar'),
   '/ECMA262/': require('../ECMA262/sidebar'),
-  '/browser/': require('../browser/sidebar')
+  '/browser/': require('../browser/sidebar'),
+  '/项目和业务/': require('../项目和业务/sidebar')
 }
---
+++ docs/项目和业务/sidebar.js
@@ -0,0 +1,9 @@
+module.exports = [
+    {
+        title: "管理系统",
+        collapsable: true,
+        children: [
+            '/项目和业务/notes/管理系统'
+        ]
+    }
+]
